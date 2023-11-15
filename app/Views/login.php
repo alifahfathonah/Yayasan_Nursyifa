1f9bb9c26bed8ede0bcd1aabbbbcd9adad19e408
gilangmukharom <gilangmukharom53@gmail.com>
2023-11-15 21:18:20 +0700

    update login responsive

--- app/Views/login.php
+++ app/Views/login.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
     <link rel="icon" href="<?= base_url('assets/images/logo.png'); ?>" type="image/x-icon">
     <title>Login</title>
 </head>
@@ -17,7 +19,9 @@
             <div class="image_logo">
                 <img src="<?php echo base_url('assets/images/logo.png') ?>" alt="">
             </div>
-            <p>Yayasan Nur Assyifa Samarinda</p>
+            <div class="text_logo">
+                <img src="<?php echo base_url('assets/images/text.png') ?>" alt="">
+            </div>
         </div>
     </nav>
 
@@ -63,6 +67,11 @@
             </form>
         </div>
     </div>
+
+    <script src="<?php echo base_url('assets/js/script.js') ?>"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
