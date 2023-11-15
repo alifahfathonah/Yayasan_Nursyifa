0404ad1b37c103ec714163b04c3a18c05c288466
gilangmukharom <gilangmukharom53@gmail.com>
2023-11-15 21:01:32 +0700

    upgrade responsive dashboard admin

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -6,6 +6,8 @@
     <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css') ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="<?= base_url('assets/images/logo.png'); ?>" type="image/x-icon">
     <title>Dashboard Admin</title>
@@ -39,6 +41,26 @@
         </div>
 
         <div class="content_dashboard">
+            <nav class="navbar navbar-dashboard navbar-expand-lg">
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="<?= base_url('dashboard'); ?>">Dashboard</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="<?= base_url('upload'); ?>">Upload</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="<?php echo base_url('Login/logout') ?>">Logout</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
             <div class="tittle_dashboard">
                 <h1>Content</h1>
                 <button onclick="window.location.href='<?= base_url('upload'); ?>'">Tambahkan Data</button>
@@ -49,6 +71,7 @@
                 </div>
             <?php endif; ?>
             <?php foreach ($tampil as $k) : ?>
+                <hr>
                 <div class="content_wrap">
                     <div class="content_image">
                         <img src="<?= base_url() . "/assets/images/" . $k['gambar']; ?>" alt="">
@@ -78,6 +101,7 @@
                         </form>
                     </div>
                 </div>
+                <hr>
             <?php endforeach; ?>
 
             <div class="pagination">
@@ -89,6 +113,10 @@
 
     <!-- Tambahkan script JavaScript -->
     <script src="<?php echo base_url('assets/js/script.js') ?>"></script>
+    <script src="<?php echo base_url('assets/js/script.js') ?>"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
--- app/Views/upload.php
+++ app/Views/upload.php
@@ -4,6 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css') ?>">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="<?= base_url('assets/images/logo.png'); ?>" type="image/x-icon">
     <title>Dashboard Upload</title>
@@ -11,6 +15,26 @@
 
 <body>
     <div class="container_dashboard">
+        <nav class="navbar navbar-dashboard navbar-expand-lg">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="<?= base_url('dashboard'); ?>">Dashboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="<?= base_url('upload'); ?>">Upload</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="<?php echo base_url('Login/logout') ?>">Logout</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
         <div class="sidebar">
             <div class="profile">
                 <img src="<?php echo base_url('assets/images/profile.png') ?>" alt="">
@@ -57,11 +81,14 @@
                     <button>Submit</button>
                 </div>
             </form>
-
-
-
         </div>
     </div>
+
+    <script src="<?php echo base_url('assets/js/script.js') ?>"></script>
+    <script src="<?php echo base_url('assets/js/script.js') ?>"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
